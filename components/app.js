d2cee10ff2afce9319788bd6652800ea05257383
Alexander Troyan <trustyone3224@gmail.com>
2021-06-16 14:45:23 +0600

    Notes - Add and Clear methods

--- components/app.js
+++ components/app.js
@@ -5,13 +5,24 @@ const App = {
         return {
             title: 'Список заметок',
             placeholderString: 'Введите название заметки',
-            inputValue: ''
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2', 'Заметка 3']
         }
     },
     methods: {
         inputChangeHandler() {
             //console.log("Something", event.target.value)
             this.inputValue = event.target.value
+        },
+        addNewNote() {
+            this.notes.push(this.inputValue)
+            this.inputValue = ''
+        },
+        clearAllNotes() {
+            while(this.notes.length > 0) {
+                this.notes.pop()
+            }
+            
         }
     }
 }
